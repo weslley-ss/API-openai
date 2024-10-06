5d92686851c36edec640b20ec75a9dde385d36ec
weslley-ss <welle.ws@gmail.com>
2024-10-06 02:21:33 -0300

    Feature:Conexão e prompt estabelecido com openAI

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
-import openai
 import os
-
-# Carregar a chave da API a partir da variável de ambiente
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
+import json
 from openai import OpenAI
-client = OpenAI()
+from datetime import datetime
 
-completion = client.chat.completions.create(
+client = OpenAI() #Variável recebida automaticamente
+
+response = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[
         {"role": "system", "content": "Você é um instrutor para um iniciante em computação e no uso de aplicações comuns ao ramo de programação"},
-        {
-            "role": "user",
-            "content": "Como eu posso encontrar todos os parâmetros possíveis para passar por um request a api da openai?"
-        }
+        {"role": "user", "content": "Me dê um road-map para eu começar construir aplicações LLM a partir da API da openai?"}
     ]
 )
 
-print(completion.choices[0].message)
+try:
+    response_json = json.dumps(response)
+except:
+    response_json = str(response)
+
+# Exibir a resposta formatada como JSON
+print(response_json)
+
+# Salvar a resposta completa em um arquivo JSON com data e hora
+now = datetime.now()
+file_name = f"resposta_{now.strftime('%Y-%m-%d_%H-%M')}.json"
+
+with open(file_name, 'w') as json_file:
+    json_file.write(response_json)
+
+print(f"A resposta foi salva em {file_name}")
